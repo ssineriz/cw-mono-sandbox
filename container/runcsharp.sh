170710233a581702b9aa3f4b9a0920c0fa1d7525
Jacques Sineriz <ssineriz@gmail.com>
2013-11-26 14:37:13 +0100

    Initial commit

--- container/runcsharp.sh
+++ container/runcsharp.sh
@@ -1,14 +1,17 @@
- #!/bin/bash
-TMP=$(/bin/mktemp)
-out=$(/usr/bin/timeout -k 4s 3s /usr/local/bin/csharp 2> "$TMP" | sed -e 's/\"/\\\"/g)
-timeout=0
- if   ["$?" -eq 124]
-	# Timeout
-	then
-		err="Code Timeout"
-		timeout=1
-	else
-		err=$(cat "$TMP" | sed -e 's/\"/\\\"/g)	
+#!/bin/bash
+TMP_ERR=$(/bin/mktemp)
+TMP_OUT=$(/bin/mktemp)
+/usr/bin/timeout -k 4s 3s /usr/local/bin/csharp 2> "$TMP_ERR" > "$TMP_OUT"
+if   [ $? -eq 124 ]  # Timeout
+then
+	err="Code Timeout"
+	out=""
+	timeout="true"
+else
+	err=$(cat "$TMP_ERR" | sed -e 's/\"/\\\"/g')
+	out=$(cat "$TMP_OUT" | sed -e 's/\"/\\\"/g')
+	timeout="false"
 fi
-rm "$TMP"
+rm "$TMP_ERR"
+rm "$TMP_OUT"
 echo "{\"out\":\"${out}\", \"err\":\"${err}\", \"timeout\":${timeout}}"
