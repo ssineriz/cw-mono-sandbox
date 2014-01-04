4d467754f549500aec88071c13764be490c1b65f
Jacques Sineriz <ssineriz@gmail.com>
2014-01-04 18:03:07 +0100

    Enhanced security

--- container/prepare.sh
+++ container/prepare.sh
@@ -1,28 +1,5 @@
 # used to tackle the 42 layers limit in docker 
 
-# make sure the package repository is up to date
-echo "deb http://archive.ubuntu.com/ubuntu precise main universe" > /etc/apt/sources.list
-apt-get update
-
-# install ucspi-tcp for tcpserver
-apt-get install -y ucspi-tcp
-
-# install iptables for basic firewalling
-apt-get install -y iptables
-
-# get server.sh
-nc https://github.com/ssineriz/cw-mono-sandbox/raw/master/container/server.sh > /usr/local/bin/server.sh
-
-# get runcsharp.sh
-nc https://github.com/ssineriz/cw-mono-sandbox/raw/master/container/runcsharp.sh > /usr/local/bin/runcsharp.sh
-
-# get ServiceStack.Text.dll
-nc https://github.com/ssineriz/cw-mono-sandbox/raw/master/lib/ServiceStack.Text.dll > /usr/local/lib/mono/4.5/ServiceStack.Text.dll
-
-# get CodeWars.CSharp.TestFramework.dll
-# this is the compiled version of https://github.com/edokan/kata-test-framework-csharp (without test units and debug code)
-nc https://github.com/ssineriz/cw-mono-sandbox/raw/master/lib/CodeWars.CSharp.TestFramework.dll > /usr/local/lib/mono/4.5/CodeWars.CSharp.TestFramework.dll
-
 # set execute permissions
 chmod +x /usr/local/bin/server.sh
 chmod +x /usr/local/bin/runcsharp.sh
